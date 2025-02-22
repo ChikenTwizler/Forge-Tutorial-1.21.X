88d544def7d5778a7100b25afcb822c77bc59daa
mongo <morganvest97@gmail.com>
2025-02-22 03:56:02 -0500

    Very important Changes!!

---
+++ src/main/java/net/mongo/tutorialmod/item/Moditems.java
@@ -0,0 +1,10 @@
+package net.mongo.tutorialmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+
+public class Moditems
+{
+    public static final DeferredRegister<Item> ITEMS =
+
+}
